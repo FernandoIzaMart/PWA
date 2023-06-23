12a202bf33a10c02a92bc7015fdc8def9b2d08f7
FernandoIzaMart <ferizamart97@gmail.com>
2023-06-23 07:53:37 -0600

    V3_PWA_PN

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,30 @@
-importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js");
-importScripts("https://www.gstatic.com/firebasejs/7.2.1/firebase-analytics.js");
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js');
 
-firebase.initializeApp({
-    messagingSenderId: "593376484217"
-  });
+const firebaseConfig = {
+    apiKey: "AIzaSyB10kyter7oJYYJ2RoCBf2Fa15B4wO79YA",
+    authDomain: "pushnotification-dba8c.firebaseapp.com",
+    projectId: "pushnotification-dba8c",
+    storageBucket: "pushnotification-dba8c.appspot.com",
+    messagingSenderId: "593376484217",
+    appId: "1:593376484217:web:3bfef690feab616ee5ada8",
+};
 
-  
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-
-messaging.setBackgroundMessageHandler(payload => {
-    const notification = JSON.parse(payload.data.notification);
-    const notificationTitle = notification.title;
-    const notificationOptions = {
-      body: notification.body
+messaging.onBackgroundMessage((payload) => {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    
+    const message = payload.data.json();
+    const title = message.notification.title;
+    const options = {
+      body: message.notification.body,
+      icon: 'path/to/notification-icon.png',
+      badge: 'path/to/notification-badge.png',
+      // Otras opciones de configuración
     };
-    //Show the notification :)
-    return self.registration.showNotification(
-      notificationTitle,
-      notificationOptions
-    );
-  });
+
+
+    self.registration.showNotification(title, options);
+}); 
--- sw.js
+++ sw.js
@@ -1,74 +1,30 @@
-importScripts('assets/lib/cache-polyfill.js');
+// Define el nombre de la caché actual
+const CACHE_NAME = 'my-cache';
 
-let CACHE_VERSION = 'app-v0.00';
-// Agregamos las rutas de todos los archivos que se trabajaran sin conexión
-let CACHE_FILES = [
-  './',
-  'index.html',
-  'assets/style/style.css',
-  'assets/script/script.js',
-  'assets/lib/cache-polyfill.js',
-  'assets/lib/bootstrap.bundle.min.js',
-  'assets/image/icon-144.png',
-  'assets/image/app-store-badge.svg',
-  'assets/image/demo-screen.mp4',
-  'assets/image/favicon.ico',
-  'assets/image/google-play-badge.svg',
-  'assets/image/tnw-logo.svg',
-  'assets/image/photo-1620207418302-439b387441b0.jpg',
-  'assets/image/photo-1620912189865-1e8a33da4c5e.jpg',
-];
+// Instala el Service Worker
+self.addEventListener('install', event => {
+  console.log('Instalado el SW');
+});
 
-self.addEventListener('install', function (event) {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_VERSION)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(CACHE_FILES);
-            })
-    )
-})
-
-self.addEventListener('fetch', function (event) {
-    let online = navigator.onLine
-    if (!online) {
-        event.respondWith(
-            caches.match(event.request).then(function (res) {
-                if (res) {
-                    return res;
-                }
-            })
-        )
-    }
-})
-
-self.addEventListener('activate', function(event){
-    event.waitUntil(
-        caches.keys().then(function(keys){
-            return prompt.all(keys.map(function(keys, i){
-                if(keys !== CACHE_VERSION){
-                    return caches.delete(keys[i]);
-                }
-            }))
+// Activa el Service Worker
+self.addEventListener('activate', event => {
+  console.log('SW Activado');
+  
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            // Borra la caché si no coincide con el nombre de la caché actual
+            return caches.delete(cacheName);
+          }
         })
-    )
-})
+      );
+    })
+  );
+});
 
-
-
-self.addEventListener('push', (event) => {
-    const data = event.data.json();
-    const { title, body } = data;
-  
-    self.registration.showNotification(title, {
-      body,
-    });
-  });
-  
-  self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    // Aquí puedes manejar el evento de clic en la notificación
-  });
-  
-
+// Intercepta y responde a las solicitudes fetch
+self.addEventListener('fetch', event => {
+  console.log('Fetch', event);
+});
